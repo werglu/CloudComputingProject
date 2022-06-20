0214ae97174c01044f498cea98a71238b58cc3d3
290450 (Weronika Głuszczak) <01130577@pw.edu.pl>
2022-06-20 18:29:41 +0200

    cleanup

--- Project2/Project2/ClientApp/src/app/home/home.component.ts
+++ Project2/Project2/ClientApp/src/app/home/home.component.ts
@@ -19,29 +19,10 @@ export class HomeComponent {
                 }
                 return 1;
             });
-            //this.posts = p;
         });
-        //this.posts = [
-        //    {
-        //        id: '1',
-        //        text: 'teteyd',
-        //        isPinned: false,
-        //        createdDate: new Date(),
-        //    },
-        //    {
-        //        id: '2',
-        //        text: 'ewrw',
-        //        isPinned: true,
-        //        createdDate: new Date(),
-        //    }];
     }
 
     public delete(post: Post) {
-        //const index = this.posts.indexOf(post, 0);
-        //if (index > -1) {
-        //    this.posts.splice(index, 1);
-        //}
-
         this.postService.deletePost(post.id).subscribe(x => {
             const index = this.posts.indexOf(post, 0);
             if (index > -1) {
